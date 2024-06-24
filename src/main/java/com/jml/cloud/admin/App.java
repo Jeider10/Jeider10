110b19ac75a50c4a0580f9a09c8792df0fa2bd33
Jeider Montiel <jmontiel@cobiscorp.com>
2024-06-24 15:05:42 -0500

    Initial commit for sistema-admin-instituciones

---
+++ src/main/java/com/jml/cloud/admin/App.java
@@ -0,0 +1,13 @@
+package com.jml.cloud.admin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
